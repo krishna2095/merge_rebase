99fee3481475f0a04ca5a8b0bf8678460bcf3a88
krishna2095 <7029sk@gmail.com>
2020-08-29 13:48:58 +0530

    performing the reational operators using conditional statements

--- init.py
+++ init.py
@@ -1,3 +1,21 @@
 a = 34
 b = 43 
 print(a+b)
+
+#perfroming the arithmetic operations 
+
+print (a-b)
+print(a*b)
+print(b/a)
+
+#performing the relational operators using conditional statements 
+
+if(10>5):
+    print("multiple of 5 is greater then 5")
+
+if(5==5):
+    print("equality check operator")
+
+
+
+
